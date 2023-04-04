c60ce415bef90091b9292dcd2e3e1b2fb43c0cb4
madikks <madiar0104@gmail.com>
2023-04-04 22:40:16 +0600

    sceleton

---
+++ kinopoiskApp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from pytils.translit import slugify
+
+class Genre(models.Model):
+    genreName = models.CharField("Жанр", max_length=255, null=True, blank=True)
+    slug = models.SlugField(unique=True, blank=True, editable=False)
+
+    def __str__(self):
+        return self.genreName
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.genreName)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+
+class Movie(models.Model):
+    movieName = models.CharField("Название фильма", max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE, verbose_name="Жанр", null=True, blank=True)
+    year = models.IntegerField("Год", null=True, blank=True)
+    country = models.CharField("Страна", max_length=255, null=True, blank=True)
+    producer = models.CharField("Продюссер", max_length=255, null=True, blank=True)
+    description = models.TextField("Описание", null=True, blank=True)
+    cover = models.CharField("Ссылка на обои", max_length=500, null=True, blank=True)
+    poster = models.CharField("Ссылка на постер", max_length=500, null=True, blank=True)
+    movieLink = models.CharField("Ссылка на фильм", max_length=500, null=True, blank=True)
+    isTopTen = models.BooleanField("Топ-10", default=False, null=True, blank=True)
+    isRecommended = models.BooleanField("Рекомендуется", default=False, null=True, blank=True)
+    rating = models.FloatField("Рейтинг",default=1.0, null=True, blank=True)
+
+    def __str__(self):
+        return self.movieName
+
+    class Meta:
+        verbose_name = "Фильм"
+        verbose_name_plural = "Фильмы"
---
+++ kinopoiskApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homePage, name='homePage'),
+    path('user/sign-up', views.signupPage, name='signupPage'),
+    path('user/login', views.loginPage, name='loginPage'),
+    path('user/logout', views.logoutUser, name='logoutUser'),
+    path('movies/all/', views.allMoviesPage, name='allMoviesPage'),
+    path('movies/all/<slug:slug>/', views.allMoviesPage, name='filterMoviesPage'),
+    path('movies/top/', views.topMoviesPage, name='topMoviesPage'),
+    path('movie/detail/<int:pk>/', views.movieDetailPage, name='movieDetailPage'),
+]
